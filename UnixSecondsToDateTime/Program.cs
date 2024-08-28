5733600a29f5ec362291d7ad10fb62f6d5749031
Ar6yZuK <Ar6yZuK@gmail.com>
2024-08-28 05:09:35 +0300

    Implemented

---
+++ UnixSecondsToDateTime/Program.cs
@@ -0,0 +1,29 @@
+﻿while (true)
+{
+	Console.Write("Write unix seconds:");
+	string? read = Console.ReadLine();
+	if (read is null)
+		return;
+
+	if (!long.TryParse(read, out var unixSeconds))
+	{
+		Console.WriteLine("Not a number");
+		continue;
+	}
+
+	try
+	{
+		Console.WriteLine(DateTimeOffset.FromUnixTimeSeconds(unixSeconds).ToLocalTime().ToString());
+	}
+	catch (ArgumentOutOfRangeException)
+	{
+		try
+		{
+			Console.WriteLine($"Too long, maybe you wrote in milliseconds: {DateTimeOffset.FromUnixTimeMilliseconds(unixSeconds).ToLocalTime()}");
+		}
+		catch (ArgumentOutOfRangeException)
+		{
+			Console.WriteLine("Not a date or not a unix time");
+		}
+	}
+}
